a68385d7d8e6241129f9cae135cfd19303f53041
camphor-h <79199281+camphor-h@users.noreply.github.com>
2026-04-13 00:18:58 +0800

    copyright updated

--- kbpaste.cc
+++ kbpaste.cc
@@ -1,6 +1,6 @@
 /*
  * kbpaste - Keyboard Simulated Paste Tool
- * Copyright (C) 2026 FogRain
+ * Copyright (C) 2026 PolarCamphor
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
